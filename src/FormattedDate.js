837bfc13dc975b81af302721129ad3f8cb9d0a46
ingekn <133350400+ingekn@users.noreply.github.com>
2023-11-19 20:11:34 +0000

    Fixed error with time

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -14,10 +14,10 @@ export default function FormattedDate(props) {
   let hours = props.date.getHours();
   let minutes = props.date.getMinutes();
   if (hours < 10) {
-    hours = `0{hours}`;
+    hours = `0${hours}`;
   }
   if (minutes < 10) {
-    minutes = `0{minutes}`;
+    minutes = `0${minutes}`;
   }
 
   return (
